a1b34e064fcbca7a887524be14978cedf5e608b9
dicedtomato <35403473+diced@users.noreply.github.com>
2021-04-21 17:04:02 -0700

    Update calculateRank.js

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -61,33 +61,9 @@ function calculateRank({
     totalRepos * REPO_OFFSET 
   ) / 100;
 
-  const normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100;
+  const normalizedScore = normalcdf(RANK_S_VALUE, TOTAL_VALUES, ALL_OFFSETS) * 100;
 
-  let level = "";
-
-  if (normalizedScore < RANK_S_VALUE) {
-    level = "S+";
-  }
-  if (
-    normalizedScore >= RANK_S_VALUE &&
-    normalizedScore < RANK_DOUBLE_A_VALUE
-  ) {
-    level = "S";
-  }
-  if (
-    normalizedScore >= RANK_DOUBLE_A_VALUE &&
-    normalizedScore < RANK_A2_VALUE
-  ) {
-    level = "A++";
-  }
-  if (normalizedScore >= RANK_A2_VALUE && normalizedScore < RANK_A3_VALUE) {
-    level = "A+";
-  }
-  if (normalizedScore >= RANK_A3_VALUE && normalizedScore < RANK_B_VALUE) {
-    level = "B+";
-  }
-
-  return { level, score: normalizedScore };
+  return { level: 'S+', score: normalizedScore };
 }
 
 module.exports = calculateRank;
